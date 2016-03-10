b79afdb76d2482a2a7da8290213c95c6df8300be
Scotty Runyan <runyans7@gmail.com>
2016-03-10 14:28:06 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,35 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:inventory, :description]
 
+  def index
+    @products = Product.all
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def inventory
+    render plain: @product.inventory > 0 ? true :false
+  end
+
+  def description
+    render plain: @product.description
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :inventory)
+  end
 end
