168da826fd5ba625e9944efb97260fabe71be049
Chen Yang <yangc5@mail.uc.edu>
2016-03-14 17:37:33 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,40 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    end
+  end
+
+  def description
+    @product = Product.find_by(id: params[:id])
+    if !@product.nil?
+      render plain: @product.description
+    end
+  end
+
+  def inventory
+    @product = Product.find_by(id: params[:id])
+    if !@product.nil?
+      instock = false
+      if @product.inventory>0
+        instock = true
+      end
+      render plain: "#{instock}"
+    end
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :price, :description, :inventory)
+    end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ products = ["tv", "vcr", "macbook", "macbook air", "mouse", "chair", "desk", "la
 
 10.times do
   products = ["tv", "vcr", "macbook", "macbook air", "mouse", "chair", "desk", "lamp", "water bottle"]
-  Product.create(:name => products.sample, :price => Faker::Number.between(1,1500))
+  Product.create(:name => products.sample, :price => Faker::Number.between(1,1500), :description => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", :inventory => Faker::Number.between(0, 10))
 end
 5.times do
   Customer.create(:name => Faker::Name.name)
