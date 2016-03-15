304ed84d7d29085503eb9277070c5db013d9ff91
macgeek30 <mike25ko@gmail.com>
2016-03-15 21:36:44 +0000

    Done.

---
+++ db/migrate/006_add_description_and_inventory_to_products.rb
@@ -0,0 +1,6 @@
+class AddDescriptionAndInventoryToProducts < ActiveRecord::Migration
+  def change
+    add_column :products, :description, :string
+    add_column :products, :inventory, :integer
+  end
+end
