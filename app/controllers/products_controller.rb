b978167cb387def910098b8624a9943ce4e997e8
Marco Tsui <marcotsui2003@yahoo.com>
2016-04-08 13:33:20 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,36 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+  
+
+  def create
+    Product.create(product_params)
+    redirect_to products_path
+  end
+
+  def inventory
+    @product = Product.find_by(id: params[:id])
+    if @product.inventory > 0
+      render plain: 'true'
+    else
+      render plain: 'false'
+    end
+    #return
+  end
+
+  def description
+    @product = Product.find_by(id: params[:id])
+    render plain: @product.description
+  end
+
+
+
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :description, :inventory, :price)
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,3 +2,16 @@ class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
 end
+
+=begin
+
+create_table "products", force: :cascade do |t|
+  t.string   "name"
+  t.integer  "price"
+  t.datetime "created_at",  null: false
+  t.datetime "updated_at",  null: false
+  t.text     "description"
+  t.integer  "inventory"
+end
+
+=end
