208db8eccb830e1687ecd2bc303407cccfc7c6f6
pickledyamsman <pickledyamsman@gmail.com>
2016-04-20 18:52:17 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,34 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
 
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to products_path
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    if product.inventory > 0
+      render plain: "true"
+    else
+      render plain: "false"
+    end
+  end
+
+  def description
+    product = Product.find(params[:id])
+    if product
+      render plain: product.description
+    else
+      render plain: "Product not found."
+    end
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:inventory, :description, :price)
+    end
 end
