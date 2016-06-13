595c26e8e413b63d4acddc615a5dbfd295147d6a
agdavid <antonio.g.david@gmail.com>
2016-06-13 10:05:42 -0400

    pass test for products#create for a new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,23 @@
 class ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
 
+  def create
+    @product = Product.create(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render "new"
+    end
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :price, :description, :inventory)
+    end
 end
