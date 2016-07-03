d24ff5962b2e8c6bf2c5efea7d861dd44fcf37a0
github username <mendelblesofsky@gmail.com>
2016-07-02 23:24:29 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,37 @@
 class ProductsController < ApplicationController
+	before_action :find_product, only: [:description, :inventory]
 
+	def index
+		@products = Product.all
+	end
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+		if @product = Product.create(product_params)
+			redirect_to products_path
+		else
+			render :new
+		end
+	end
+
+	def description
+		render plain: @product.description
+	end
+
+	def inventory
+		render plain: (@product.inventory > 0).to_s
+	end
+
+	def find_product
+		@product = Product.find(params[:id])
+	end
+
+	private
+
+	def product_params
+		params.require(:product).permit(:name, :price, :description, :inventory)
+	end
 end
