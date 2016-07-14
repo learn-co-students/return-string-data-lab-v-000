9ae048d457d875db54af0ae7d9ecce19ee1d7242
McN33ly <rmcneely23@gmail.com>
2016-07-14 09:51:37 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,33 @@
 class ProductsController < ApplicationController
 
+  def create
+    Product.create(product_params)
+    # binding.pry
+    redirect_to products_path
+  end
+
+  def inventory
+    product = Product.find_by_id(params[:id])
+    if product.inventory > 0
+      render plain: true
+    else
+      render plain: false
+    end
+
+  end
+
+  def description
+    product = Product.find_by_id(params[:id])
+    render plain: product.description
+    # binding.pry
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :inventory, :price, :description)
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'selenium-webdriver'
+Selenium::WebDriver::Firefox::Binary.path='/home/ryan/Downloads/firefox/firefox-bin'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
