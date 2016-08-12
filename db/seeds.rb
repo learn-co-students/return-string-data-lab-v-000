694ea0001c304dfbfdb0505440b19827c219bf66
reichardn <reichardn@gmail.com>
2016-08-12 00:32:57 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 products = ["tv", "vcr", "macbook", "macbook air", "mouse", "chair", "desk", "lamp", "water bottle"]
 
+d = "jQuery is a fast, small, and feature-rich JavaScript library. It makes things like HTML document traversal and manipulation, event handling, animation, and Ajax much simpler with an easy-to-use API that works across a multitude of browsers. If you're new to jQuery, we recommend that you check out the jQuery Learning Center"
+
 10.times do
   products = ["tv", "vcr", "macbook", "macbook air", "mouse", "chair", "desk", "lamp", "water bottle"]
-  Product.create(:name => products.sample, :price => Faker::Number.between(1,1500))
+  inventory = [0,1,2,3]
+  Product.create(:name => products.sample, :price => Faker::Number.between(1,1500), :description => d, :inventory => inventory.sample)
 end
 5.times do
   Customer.create(:name => Faker::Name.name)
