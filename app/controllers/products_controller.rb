e801c0328251a69fb906780198751ffd2c9ff13e
natasha carlyon <natasha.carlyon@gmail.com>
2016-08-16 00:02:20 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,36 @@
 class ProductsController < ApplicationController
 
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(products_params)
+    @product.save
+    redirect_to products_path
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    if product.inventory == 0
+      render plain: false
+    else
+      render plain: true
+    end
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  private
+
+  def products_params
+    params.require(:product).permit(:name, :price, :inventory, :description)
+  end
 end
