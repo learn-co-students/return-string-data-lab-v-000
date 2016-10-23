50583db6ec90149fd02bdc4abcf4438ed431b86f
Jesse Novotny <jessenovotny@gmail.com>
2016-10-23 12:01:28 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,41 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+    @products.first.inventory = 0
+    @products.first.save
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    Product.create(product_params)
+    redirect_to products_path
+  end
+
+  def description
+    set_product
+    render plain: @product.description
+  end
+
+  def inventory
+    set_product
+    if @product.inventory == 0
+      render plain: "false"
+    else
+      render plain: "true"
+    end
+  end
+
+private
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :inventory, :price)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,21 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-products = ["tv", "vcr", "macbook", "macbook air", "mouse", "chair", "desk", "lamp", "water bottle"]
+# products = ["tv", "vcr", "macbook", "macbook air", "mouse", "chair", "desk", "lamp", "water bottle"]
 
-10.times do
-  products = ["tv", "vcr", "macbook", "macbook air", "mouse", "chair", "desk", "lamp", "water bottle"]
-  Product.create(:name => products.sample, :price => Faker::Number.between(1,1500))
-end
-5.times do
-  Customer.create(:name => Faker::Name.name)
-end
+# 10.times do
+#   products = ["tv", "vcr", "macbook", "macbook air", "mouse", "chair", "desk", "lamp", "water bottle"]
+#   Product.create(:name => products.sample, :price => Faker::Number.between(1,1500))
+# end
+# 5.times do
+#   Customer.create(:name => Faker::Name.name)
+# end
 
-5.times do
-  Invoice.create
-end
-10.times do
-  order = Order.create(:customer_id => Customer.all.collect(&:id).sample, :invoice_id => Invoice.all.collect(&:id).sample)
-  order.products << Product.find(Product.all.collect(&:id).sample)
-end
+# 5.times do
+#   Invoice.create
+# end
+# 10.times do
+#   order = Order.create(:customer_id => Customer.all.collect(&:id).sample, :invoice_id => Invoice.all.collect(&:id).sample)
+#   order.products << Product.find(Product.all.collect(&:id).sample)
+# end
 
+
+Product.all.each do |p|
+  p.description = Faker::Hipster.sentence
+  p.save!
+end
