deb096faedd195ad60167241ae1d355c67a58ba6
Kailee <kaileeagray@gmail.com>
2016-10-27 18:33:19 -0500

    Done.

--- db/migrate/20160125172335_create_products.rb
+++ db/migrate/20160125172335_create_products.rb
@@ -3,6 +3,8 @@ class CreateProducts < ActiveRecord::Migration
     create_table :products do |t|
       t.string :name
       t.integer :price
+      t.integer :inventory
+      t.string :description
       t.timestamps null: false
     end
   end
