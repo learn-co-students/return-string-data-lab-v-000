95f6c8ba53af2c3f09cf1a1f01609ec40bd1c8a7
KerryAlsace <kerrydougherty@gmail.com>
2016-11-04 15:56:24 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,38 @@
 class ProductsController < ApplicationController
+	def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)    
+    if @product.save
+    	flash[:notice] = "Product Added"
+
+    	redirect_to products_path
+    else
+    	flash[:alert] = "Could not add product"
+
+    	render :new
+    end
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    render plain: product.inventory > 0 ? true : false
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  private
+	  def product_params
+	    params.require(:product).permit(:name, :description, :inventory)
+	  end
 
 end
