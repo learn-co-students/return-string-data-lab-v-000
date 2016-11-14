affd354836931600de000af250b4c29f5620d398
Traci7822 <Tracir@gmail.com>
2016-11-14 14:26:21 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,20 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
 
+  def create
+    @product = Product.create(name: params[:name], description: params[:description])
+    redirect_to products_path
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    render plain: product.inventory
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
 end
