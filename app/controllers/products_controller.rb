4c10c5e79374ea76c675c5a1bdefefb71f7862f0
Yechiel K <chilikkal@gmail.com>
2017-01-18 22:35:32 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,33 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    Product.create(product_params)
+    redirect_to products_path
+  end
+
+  def inventory
+    product = Product.find_by_id(params[:id])
+    available = product.inventory > 0
+    render plain: available
+  end
+
+  def description
+    product = Product.find_by_id(params[:id])
+    render plain: product.description
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :inventory)
+  end
+
 end
