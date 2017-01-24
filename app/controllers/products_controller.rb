459d3299bf2ffde43327e2b0a51adeff12b60e61
MIKE-RANDELL <michael.james.randell@gmail.com>
2017-01-23 19:41:03 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,34 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
 
+  def inventory
+    product = Product.find(params[:id])
+    if product.inventory > 0
+      render plain: "true"
+    else
+      render plain: "false"
+    end
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  def new
+
+  end
+
+  def create
+    product = Product.create(product_params_checker)
+    redirect_to products_path
+  end
+
+  private
+
+  def product_params_checker
+    params.require(:product).permit(:name, :price, :description, :inventory)
+  end
 end
