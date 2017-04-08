3dd2e5e14ff8be42927cae4280b38439f4b509eb
Nathan <admin@bigrocketgames.com>
2017-04-08 07:34:33 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,11 +10,25 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.create(product_params)
-    products_path
+    redirect_to products_path
   end
 
   def show
     @product = Product.find(params[:id])
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    if product.inventory == 0
+      render plain: "false"
+    else
+      render plain: "true"
+    end
   end
 
   private
