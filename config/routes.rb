2496c0b20f7eb5cde9a4c6a7cda1ea523fe6f414
Malind Kowalski <malind.nikulski@gmail.com>
2017-06-07 15:52:11 -0600

    +productcon#methods, .js, routes, _product/index/new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'home', to: 'static#home'
+  get 'products/:id/description', to: 'products#description'
+  get 'products/:id/inventory', to: 'product#inventory'
+
   resources :orders
   resources :invoices
+  resources :products
 end
--- db/migrate/20160125172335_create_products.rb
+++ db/migrate/20160125172335_create_products.rb
@@ -3,6 +3,8 @@ class CreateProducts < ActiveRecord::Migration
     create_table :products do |t|
       t.string :name
       t.integer :price
+      t.integer :inventory, default: 0
+      t.string :description
       t.timestamps null: false
     end
   end
