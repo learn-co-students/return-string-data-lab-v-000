ea5a9c4aa00dfc23880d1b9932ce9950be66890f
Malind Kowalski <malind.nikulski@gmail.com>
2017-06-07 16:03:07 -0600

    completed string data return lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'home', to: 'static#home'
   get 'products/:id/description', to: 'products#description'
-  get 'products/:id/inventory', to: 'product#inventory'
+  get 'products/:id/inventory', to: 'products#inventory'
 
   resources :orders
   resources :invoices
