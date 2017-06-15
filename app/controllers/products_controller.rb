5b22778ac34b78592a6834d0297a632c193d1b54
Bob Habig <rfhab2@gmail.com>
2017-06-15 02:21:28 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,20 @@
 class ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :inventory, :description)
+  end
 end
