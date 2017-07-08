a1a041f67b14e0e08c6805a79626a30af1d491ed
Trai Compton <tlcompton2546@outlook.com>
2017-07-08 21:04:22 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,40 @@
 class ProductsController < ApplicationController
-
+    
+    def index
+       @products = Product.all 
+    end
+    
+    def new
+        @product = Product.new
+    end
+    
+    def create
+        product = Product.new(product_params
+        )
+        if product.valid?
+            product.save
+        else
+            render new
+        end
+        
+        redirect_to products_path
+    end
+    
+    def description
+       product = Product.find(params[:id]) 
+       
+       render plain: product.description
+    end
+    
+    def inventory
+        product = Product.find(params[:id])
+        
+        render plain: product.inventory > 0 ? true : false
+    end
+    
+    private
+    
+    def product_params
+        params.require(:product).permit(:name, :price, :description, :inventory)
+    end
 end
