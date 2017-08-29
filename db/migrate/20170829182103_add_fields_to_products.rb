14cd0965922aa2946e5dcff229c2f8fd3127e31e
James Fernandez <jffernan@comcast.net>
2017-08-29 11:23:43 -0700

    Update 20170829182103_add_fields_to_products.rb

--- db/migrate/20170829182103_add_fields_to_products.rb
+++ db/migrate/20170829182103_add_fields_to_products.rb
@@ -1,4 +1,8 @@
 class AddFieldsToProducts < ActiveRecord::Migration
+  
   def change
+    add_column :products, :inventory, :integer
+    add_column :products, :description, :string
   end
+  
 end
