0880b697e312f05d8233da9ffbcb673d03b81b00
github username apalski <github email address annette.drapalski@gmail.com>
2017-08-31 11:58:02 +1000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,42 @@
 class ProductsController < ApplicationController
 
+	def index
+		@products = Product.all
+	end
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.new(product_params)
+		@product.save
+		redirect_to products_path
+	end
+
+	def show
+		product_id
+	end
+
+	def description
+		product_id
+		render plain: @product.description
+	end
+
+	def inventory
+		product_id
+		result = @product.inventory != 0 ? "true" : "false"
+		render plain: result	
+	end
+
+	private
+
+	def product_params
+		params.require(:product).permit(:name, :description, :price, :inventory)
+	end
+
+	def product_id
+		@product = Product.find(params[:id])
+	end
+
 end
--- db/migrate/20160125172335_create_products.rb
+++ db/migrate/20160125172335_create_products.rb
@@ -2,7 +2,9 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :name
+      t.string :description
       t.integer :price
+      t.integer :inventory, default: 0
       t.timestamps null: false
     end
   end
