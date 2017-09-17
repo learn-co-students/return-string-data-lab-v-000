c21169221de6df6965268c119302f149a6b67aa1
Moshe Levi <mo6709@gmail.com>
2017-09-17 16:18:33 -0400

    Done.

--- db/migrate/20160125172335_create_products.rb
+++ db/migrate/20160125172335_create_products.rb
@@ -1,8 +1,10 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-      t.string :name
-      t.integer :price
+      t.string  :name
+      t.string :price
+      t.text    :description
+      t.integer :inventory
       t.timestamps null: false
     end
   end
