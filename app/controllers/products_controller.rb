e5091be1b4c04b0586807aeedbde254d8f838598
dustin <dandersonsoftware@outlook.com>
2017-10-03 00:46:05 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,36 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(products_params)
+    redirect_to products_path
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    if product.inventory > 0
+      inv = true
+    else
+      inv = false
+    end
+    render plain: inv
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  private
+
+  def products_params
+    params.require(:product).permit(:name, :price, :inventory, :description)
+  end
 end
---
+++ db/migrate/20171003000903_add_column_inventory_to_products.rb
@@ -0,0 +1,6 @@
+class AddColumnInventoryToProducts < ActiveRecord::Migration
+  def change
+    add_column :products, :inventory, :integer
+    add_column :products, :description, :string
+  end
+end
