0ea4e8b1c4ffebab77710ac53c4cac3efc621c15
curlywallst <nnoyes@comcast.net>
2017-10-03 10:13:14 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,11 +25,7 @@ class ProductsController < ApplicationController
 
   def inventory
     product = Product.find(params[:id])
-    if product.inventory > 0
-      render plain: "true"
-    else
-      render plain: "false"
-    end
+    render plain: product.inventory > 0 ? true : false
   end
 
   private
