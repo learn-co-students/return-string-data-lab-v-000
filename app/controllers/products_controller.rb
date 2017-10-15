2635d8e9e514baa13089402c527668fb83e4f22d
Matteo Piccini <gnappo.1@live.it>
2017-10-15 08:30:29 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,35 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:inventory, :description]
 
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save ? (redirect_to products_path) : (render "new")
+  end
+
+  def inventory
+    @product.inventory > 0 ? (render plain: "true") : (render plain: "false")
+  end
+
+  def description
+    render plain: @product.description
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def product_params
+      params.require(:product).permit(:name, :description, :price, :inventory)
+    end
 end
