1e84f4c797c87869ee059a05fead65d4793afb07
smithwebtek <brad@smithwebtek.com>
2017-10-30 13:20:06 -0400

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,14 @@
 Rails.application.routes.draw do
 
   get 'home', to: 'static#home'
-  get '/products/:id/description', to: 'products#description'
+  resources [:orders, :invoices]
+  resources :products, only: [:index, :show, :new, :create]
+	
+	post '/products', to: 'products#create'
+	
+	get '/products/:id/description', to: 'products#description'
   get '/products/:id/available', to: 'products#available'
   get '/products/:id/inventory', to: 'products#inventory'
-
-  post '/products', to: 'products#create'
-
-  resources :orders
-  resources :invoices
- 
-  resources :products, only: [:index, :show, :new, :create]
   get '/products/:id/description', to: 'products#description'
   get '/products/:id/inventory', to: 'products#inventory'
- 
-  resources :products, only: [:index, :new, :create]
- 
 end
