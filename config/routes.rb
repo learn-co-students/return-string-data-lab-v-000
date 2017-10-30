103e5d93699244745e9c6b138398c712561809fa
smithwebtek <brad@smithwebtek.com>
2017-10-30 13:23:06 -0400

    refactor routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
 
   get 'home', to: 'static#home'
-  resources [:orders, :invoices]
-  resources :products, only: [:index, :show, :new, :create]
-	
-	post '/products', to: 'products#create'
-	
-	get '/products/:id/description', to: 'products#description'
+  get '/products/:id/description', to: 'products#description'
   get '/products/:id/available', to: 'products#available'
   get '/products/:id/inventory', to: 'products#inventory'
-  get '/products/:id/description', to: 'products#description'
-  get '/products/:id/inventory', to: 'products#inventory'
+  resources :orders, :invoices
+  resources :products, only: [:index, :show, :new, :create]
+  post '/products', to: 'products#create'
 end
