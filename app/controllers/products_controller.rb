53ee7343b35de7b482f11dec2e1bb56f2b10a362
psnider30 <pbsnider@gmail.com>
2017-11-22 13:58:58 -0800

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,44 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def description
+    product = Product.find(params[:id])
+    if product.description
+      render plain: product.description
+    else
+      render plain: ''
+    end
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    if product.inventory
+      render plain: product.inventory > 0
+    else
+      render plain: ''
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :inventory, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
+  resources :products
+  get '/products/:id/inventory' => 'products#inventory'
+  get '/products/:id/description' => 'products#description'
 end
