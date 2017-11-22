284f2a86ac92f4bf939b65ec8230d7c1119c99d1
gb23 <gregbenj@hotmail.com>
2017-11-22 13:08:00 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,6 +17,13 @@ class ProductsController < ApplicationController
         render plain: product.description
     end
 
+    def inventory
+        result = "false"
+        product = Product.find_by_id(params[:id])
+        result = "true" if !product.inventory.nil? && product.inventory > 0  
+        render plain: result
+    end
+
     private
     def product_params
         params.require(:product).permit(:name, :description, :price, :inventory)
