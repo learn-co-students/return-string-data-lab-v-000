64942c290b68d232ae4c77ce84ef75923a700806
kaisa <kaisa.piipari@gmail.com>
2017-11-30 01:13:32 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,45 @@
 class ProductsController < ApplicationController
+    
+    def index
+        @products = Product.all
+    end
+
+    def new
+        @product = Product.new
+    end
+
+    def description
+        product = Product.find(params[:id])
+        if product.description != nil
+            render plain: product.description
+        else
+            render plain: "No description available"
+        end
+    end
+
+    def create
+        product = Product.new(product_params)
+        if product.save
+            redirect_to products_path
+        end
+    end
+
+    def inventory
+        product = Product.find(params[:id])
+        if product.inventory != nil
+            if product.inventory > 0
+                render plain: "true"
+            else
+                render plain: "false"
+            end
+        end
+    end
+
+
+    private
+
+    def product_params
+        params.require(:product).permit(:name, :price, :inventory, :description)
+    end
 
 end
