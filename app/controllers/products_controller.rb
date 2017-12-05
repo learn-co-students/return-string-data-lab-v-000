106e06963a59c1127e271a9cdfa865e3b1a5369e
Sophia Hubeny <shhubeny@gmail.com>
2017-12-05 06:25:39 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,23 +3,9 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
-  def new
-    @product = Product.new
-  end
-
-  def create
-    @product = Product.create(product_params)
-    redirect_to products_path
-  end
-
   def body
-    @product = Product.find(params[:id])
-    render plain: @product.description
+    product = Product.find(params[:id])
+    render plain: product.description
   end
 
-  private
-
-  def product_params
-    params.require(:product).permit(:name, :price, :description, :inventory)
-  end
 end
