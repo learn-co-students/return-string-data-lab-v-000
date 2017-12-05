3a2ef8b138544e4d5214e610a5a384bd154cf67a
Sophia Hubeny <shhubeny@gmail.com>
2017-12-05 07:21:06 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -29,6 +29,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :price, :description, :inventory)
+    params.require(:product).permit(:name, :price, :inventory, :description)
   end
 end
