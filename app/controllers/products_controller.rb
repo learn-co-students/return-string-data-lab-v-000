07309bb62000ca2a018913aa25e9498dee3a438b
Jim Stricker <jmstricker93@gmail.com>
2017-12-07 11:58:06 -0500

    rudimentary API endpoint for product description stubbed out

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,34 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render 'product/new'
+    end
+  end
+
+  #API ENDPOINTS
+  def body
+    @product = Product.find(params[:id])
+    render plain: @product.description
+  end
+
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :inventory)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home', to: 'static#home'
+  get '/products/:id/body', to: 'products#body'
   resources :orders
   resources :invoices
-  resources :products, only: [:index]
+  resources :products, only: [:index, :new, :create, :show]
 end
