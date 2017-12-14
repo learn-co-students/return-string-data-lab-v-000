71532a35ccab98e7b5d369a4a67dde3aa0a4669b
jake <jtb1137@gmail.com>
2017-12-14 13:52:21 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 gem 'pry'
 gem 'pry-nav'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.2.5'
+gem 'rails', '~> 5.0'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use SCSS for stylesheets
@@ -23,6 +23,8 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
+
+gem 'rails-controller-testing'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,42 @@
 class ProductsController < ApplicationController
+    before_action :product_setter, only: [:show, :description, :inventory]
+
+    def description
+        render plain: @product.description
+    end
+
+    def inventory
+        render plain: @product.inventory
+    end
+
+    def index
+        @products = Product.all
+    end
+
+    def create
+        new_product = Product.create(product_params)
+        if new_product.save
+            redirect_to products_path
+        else
+            render :new
+        end
+    end
+
+    def new
+        @product = Product.new
+    end
+
+    def show
+    end
+
+    private
+
+    def product_setter
+       @product = Product.find(params[:id]) 
+    end
+
+    def product_params
+        params.require(:product).permit(:name, :price, :inventory, :description)
+    end
 
 end
