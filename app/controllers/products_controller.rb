8ca76d5463de670deddb39790672ebcd362910a6
Monika Williams <monika.m.williams@gmail.com>
2017-12-22 11:56:43 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,44 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    @availability = product.inventory > 0
+    render plain: @availability
+  end
+
+  def new
+      @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+     if @product.save
+    redirect_to products_path
+    else
+    render new
+  end
+end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+  end
+
+  private
+
+def product_params
+     params.require(:product).permit(
+     :name,
+     :description,
+     :price,
+     :inventory)
+   end
 end
