2f85ab9239ec5f2a2de74d1ca24a224bb3ba2fb1
ndalcin <dalcin.nicole@gmail.com>
2018-01-06 21:00:10 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,51 @@
 class ProductsController < ApplicationController
 
+    before_action :set_product, only: [:show, :edit, :update, :description, :inventory]
+
+    def index
+      @products = Product.all
+    end
+
+    def show
+    end
+
+    def new
+      @product = Product.new
+    end
+
+    def create
+      @product = Product.create(product_params)
+      redirect_to products_path
+    end
+
+    def edit
+    end
+
+    def update
+      @product.update(product_params)
+      redirect_to product_path(@product)
+    end
+
+    def description
+      render plain: @product.description
+    end
+
+    def inventory
+      render plain: @product.inventory_available?
+    end
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def product_params
+      params.require(:product).permit(:name, :description, :integer, :price)
+    end
+
+
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,13 @@
 class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
+
+  def inventory_available?
+    if self.inventory == 0
+      return "false"
+    else
+      return "true"
+    end
+  end
+
 end
