57d560d9ca019eaa08995d826a863537fafd07ce
emhopkins <epoletto@lstreetc.com>
2018-02-03 15:00:37 -0600

    Done.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
+
+  def inventory_status
+  	inventory > 0 ? "Available" : "Sold Out"
+  end
 end
