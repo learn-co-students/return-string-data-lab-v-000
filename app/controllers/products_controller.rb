9a7e270ee019a735df1a48029c844efab0c3a5bf
streamedLine <dovidlwtn@gmail.com>
2018-02-07 00:26:24 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,32 @@
 class ProductsController < ApplicationController
+	def index
+		@products = Product.all
+	end
 
+	def description
+		description = Product.find(params[:id]).description
+		render :plain => description
+	end
+
+	def inventory
+		in_stock = Product.find(params[:id]).in_stock?
+		render :plain => in_stock
+	end
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.new(product_params)
+		@product.save
+
+		redirect_to products_path
+	end
+
+	private
+
+	def product_params
+		params.require(:product).permit(:name, :description)
+	end
 end
