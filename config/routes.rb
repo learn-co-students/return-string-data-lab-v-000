a9b9a185603fa854f840aacf06735be5942f21ea
Justin Daniel <justinddaniel@gmail.com>
2018-02-08 00:44:12 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get 'home', to: 'static#home'
+  root 'products#index'
+  resources :products
+  get 'products/:id/body', to: 'products#body'
+  get 'products/:id/available', to: 'products#available'
   resources :orders
   resources :invoices
+
 end
