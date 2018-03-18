51f5dde5eaa29e69abf65e3977b4163de9fe062b
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-03-18 20:37:22 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,34 @@
 class ProductsController < ApplicationController
+    
+    def new 
+        @product = Product.new
+    end 
+    
+    def create 
+        # binding.pry
+        @product = Product.create(product_params)
+        @product.save
+        redirect_to products_path
+    end 
+
+    def index
+        @products = Product.all
+    end 
+    
+    def show
+       @product = Product.find(params[:id]) 
+    
+    end 
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    #   def set_post
+    #     @post = Post.find(params[:id])
+    #   end
+    
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def product_params
+        params.require(:product).permit(:name, :price, :description, :inventory)
+    end
 
 end
