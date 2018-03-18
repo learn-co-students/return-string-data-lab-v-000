83c112d097acced8663b0ef286b5f2200940cde6
Igor Eskin <github email address>
2018-03-18 17:48:30 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,44 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    if product.inventory && product.inventory > 0
+      render plain: 'true'
+    else
+      render plain: 'false'
+    end
+  end
+
+  def create
+    @product = Product.create(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def set_product
+    product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :inventory)
+  end
+
 end
