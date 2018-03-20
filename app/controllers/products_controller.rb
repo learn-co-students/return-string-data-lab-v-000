510765afdc0d1035f62cf2402f40d21eb6748355
github username <github email address>
2018-03-20 15:01:35 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,29 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
 
+  def create
+    Product.create(products_params)
+    redirect_to products_path
+  end
+
+  def description
+    @product = Product.find(params[:id])
+    render plain: @product.description
+  end
+
+  def inventory
+    @product = Product.find(params[:id])
+    if @product.inventory > 0
+      render plain: true
+    else
+      render plain: false
+    end
+  end
+  private
+
+  def products_params
+    params.require(:product).permit(:description, :name, :price, :inventory)
+  end
 end
