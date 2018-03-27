d3db31851fd1835db9ae072e93f976cdbfa8014a
Autumn Kangas <autumnkangas@gmail.com>
2018-03-27 00:24:40 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
+  resources :products, only: [:new, :create, :index]
+
+  #power AJAX request to replace truncated content on /posts with full body of post
+  get '/products/:id/description', to: 'products#description'
+
+  get '/products/:id/inventory', to: 'products#inventory'
 end
