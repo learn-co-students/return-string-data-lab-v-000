b27eab8b942fa413eff79af218698062d2c75507
chrisjamesr <chrisjames.r@gmail.com>
2018-04-07 00:31:16 -0400

    Done.

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,31 +1,15 @@
 // $(function(){
-//   $(".js-more").on("click", function(e){
-//     let productId = $(this).data("id")
+//   $(".js-more-info").on("click", function(e){
+//     var productId = $(this).data("id")
 //     $.get(`/products/${productId}/description`, function(description){
 //       $.get(`/products/${productId}/inventory`, function(inventory){
-//         let availability = "Available"
+//         var availability = "Available"
 //         if (inventory === "false" ){
 //           availability = "Sold Out"
 //         }
-//         let productString = "<p>" + description + "</p><p> " + availability + "</p>";
+//         var productString = "<p>" + description + "</p><p> " + availability + "</p>";
 //         $("#product-" + productId).html(productString);
 //       })
 //     })
 //   })
 // })  
-
-// $(function() {
-//   $(".js-more").on("click", function() {
-//     var id = $(this).data("id");
-//     $.get("/products/" + id + "/description", function(description) {
-//       $.get("/products/" + id + "/inventory", function(inventory) {
-//         var inventoryText = "<strong>Available</strong>";
-//         if(inventory === "false"){
-//           inventoryText = "<strong>Sold Out</strong>";
-//         }
-//         var descriptionText = "<p>" + description + "</p><p>" + inventoryText + "</p>";
-//         $("#product-" + id).html(descriptionText);
-//       });
-//     });
-//   });
-// });
