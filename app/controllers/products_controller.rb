a3ea4232198cc7f246bf223953edaf30417b7ac1
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-04-11 16:51:29 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,38 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(product_params)
+    redirect_to products_path
+  end
+
+  # Add a route and action that gets just a product description as a plain string.
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  #Add another route and action that checks if inventory is available.
+  #This route should return just "true" or "false" as strings
+  def inventory
+    product = Product.find(params[:id])
+    if product.inventory != 0
+      render plain: 'true'
+    else
+      render plain: 'false'
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :inventory, :description)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
+  resources :products
+
+  # Add a route and action that gets just a product description as a plain string.
+  get 'product/:id/description', to: 'products#description'
+
+  #Add another route and action that checks if inventory is available.
+  #This route should return just "true" or "false" as strings
+  get 'product/:id/inventory', to: 'products#inventory'
+  
 end
