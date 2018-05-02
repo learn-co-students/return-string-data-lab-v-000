a8f54028f26914315c0d799a2a3ca6c00cc666dd
Chris Henry <cdhenry@gmail.com>
2018-05-02 21:05:42 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   resources :orders
   resources :invoices
   resources :products
+  get '/products/:id/description', to: 'products#description'
+  get '/products/:id/inventory', to: 'products#inventory'
+  get '/products/:id/count', to: 'products#count'
 end
