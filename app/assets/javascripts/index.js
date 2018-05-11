a43bbbefc43fd0848f4c7822503eac0084d7c766
NolanHughes <Nhughes987@gmail.com>
2018-05-11 11:08:11 -0500

    Done.

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,8 @@
+// $(function () {
+//   $(".js-more").on('click', function() {
+//     const id = $(this).data("id");
+//     $.get("/products/" + id + "/description", function(desc) {
+//       $(`#description-` + id).text(desc);
+//     });
+//   });
+// });
