4449ffe3c4ac9a50aad1846993887f3c0ed20929
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-05-22 09:33:44 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
 
   def index
+    @products = Product.all
   end
 
   def new
@@ -8,6 +9,18 @@ class ProductsController < ApplicationController
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      redirect_to new_product_path
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :inventory)
   end
 
 end
