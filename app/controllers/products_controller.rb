d755c89b63b7db02d3ed4050de1025a487da2ced
github username <github email address>
2018-05-26 14:04:06 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,37 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :inventory, :description]
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+     @product = Product.new
+   end
+
+  def create
+    @product = Product.create(product_params)
+    @product.save
+    redirect_to products_path
+  end
+
+  def inventory
+    render plain: @product.inventory > 0
+  end
+
+  def description
+    render plain: @product.description
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def product_params
+      params.require(:product).permit(:name, :description, :inventory)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
+
+  resources :products, only: [:index, :show, :new, :create, :edit]
+  # get '/products/:id/body', to: 'products#body'
+  get '/products/:id/inventory', to: 'products#inventory'
+  get '/products/:id/description', to: 'products#description'
 end
