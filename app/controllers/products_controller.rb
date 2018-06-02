0f1978ebd9741e8f526726308c18414d58637714
tpetersen0308 <travis.e.petersen@gmail.com>
2018-06-01 19:02:51 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,19 @@
 class ProductsController < ApplicationController
+  
+  def index
+    @products = Product.all
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+
+    render plain: product.inventory
+  end
+
+  def description
+    product = Product.find(params.require(:product_id))
+
+    render plain: product.description
+  end
 
 end
