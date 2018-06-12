3d653e62564ef435010d6e8762cf73f128afeb06
Dalma Boros <dalma.boros@gmail.com>
2018-06-12 14:58:09 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,29 @@
 class ProductsController < ApplicationController
 
+  def new
+    #
+  end
+
+  def create
+    @product = Product.new
+    if @product.save
+      redirect_to products_path
+    else
+      render new_product_path
+    end
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    render plain: !!(product.inventory > 0)
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
 end
