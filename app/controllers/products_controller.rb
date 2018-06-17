a4bc12c11f9802476f61e7458301f092e53b12cc
Robert Hughes <roberts.ginger.email@gmail.com>
2018-06-17 08:20:45 +0100

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,27 @@
 class ProductsController < ApplicationController
+  def create
+    product = Product.new(product_params)
+    product.save
+    redirect_to products_path
+  end
 
+  def inventory
+    product = Product.find(params[:id])
+    render plain: (product.inventory != 0).to_s
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :inventory, :description)
+  end
 end
