eb7e80cc690c9fe7a3c91008450229a92b1eeb5b
Kelsey Fecho <kfecho30@gmail.com>
2018-07-01 18:37:10 +0000

    Done.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,14 @@
 class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
+  attr_accessor :description, :inventory
+
+  def description
+    return "description goes here"
+  end
+
+
+  def inventory
+    self.all.count
+  end
 end
