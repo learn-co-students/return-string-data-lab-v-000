238e4bbab732ccfc5ff603acb9cc692b0f5d5634
Sam Marshall <samuel.r.marshall@gmail.com>
2018-07-01 13:27:31 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,36 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(product_params)
+    redirect_to products_path
+  end
+
+  def description
+    product = set_product
+    render plain: product.description
+  end
+
+  def inventory
+    product = set_product
+    render plain: product.inventory>0
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :inventory)
+  end
+
+  def set_product
+    Product.find(params[:id])
+  end
+
 end
---
+++ db/migrate/20180701162004_add_inventory_to_products.rb
@@ -0,0 +1,6 @@
+class AddInventoryToProducts < ActiveRecord::Migration
+  def change
+    add_column :products, :inventory, :integer
+    add_column :products, :description, :text
+  end
+end
