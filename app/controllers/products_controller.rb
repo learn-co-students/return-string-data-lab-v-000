950deabf1894388aabcca2e2100b235eafc480f9
ronald vilorio <soccer23415@gmail.com>
2018-07-03 02:44:46 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,22 @@
 class ProductsController < ApplicationController
-
+  def create
+    @product = Product.create(product_params)
+    redirect_to products_path
+  end
+  def index
+    @products = Product.all
+  end
+  def inventory
+    product = Product.find_by(id: params[:id])
+    render plain: product.inventory != 0
+  end
+  def description
+    # binding.pry
+    @product = Product.find_by(id: params[:id])
+    render plain: @product.description
+  end
+    private
+    def product_params
+      params.require(:product).permit( :inventory, :description, :name, :price,)
+    end
 end
