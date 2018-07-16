537c1fae2e2482de6e6098e410d02f8b5973f051
Sean Oughton <seanoughton@gmail.com>
2018-07-16 20:11:39 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home', to: 'static#home'
+  get 'products/:id/description', to: "products#description"
   resources :orders
   resources :invoices
   resources :products
