004ef2dda0b7eba65e8d6fa04c1eabec146deac2
Sean Oughton <seanoughton@gmail.com>
2018-07-16 19:35:27 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -40,9 +40,11 @@ ActiveRecord::Schema.define(version: 20160125172450) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
+    t.string   "description"
+    t.integer  "inventory"
     t.integer  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
