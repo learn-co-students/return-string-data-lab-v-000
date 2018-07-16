5abca45cde18ac06d968a4914d20e592f227f38e
Joanne Lee <vernapii@gmail.com>
2018-07-16 10:31:48 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,9 @@
 class ProductsController < ApplicationController
 
+  def create
+   redirect_to 'index'
+  end
+
+  def index
+  end
 end
---
+++ db/migrate/20180716172314_add_description_inventory_product.rb
@@ -0,0 +1,6 @@
+class AddDescriptionInventoryProduct < ActiveRecord::Migration
+  def change
+    add_column :products, :description, :string
+    add_column :products, :inventory, :string
+  end
+end
