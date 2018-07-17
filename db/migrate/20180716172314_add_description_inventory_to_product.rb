3049fcf4abbc8a7da9283ef4e59449654b60e9b5
Joanne Lee <vernapii@gmail.com>
2018-07-16 18:40:18 -0700

    Done.

--- db/migrate/20180716172314_add_description_inventory_to_product.rb
+++ db/migrate/20180716172314_add_description_inventory_to_product.rb
@@ -1,6 +1,6 @@
 class AddDescriptionInventoryToProduct < ActiveRecord::Migration
   def change
     add_column :products, :description, :string
-    add_column :products, :inventory, :float
+    add_column :products, :inventory, :integer
   end
 end
