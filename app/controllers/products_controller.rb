ca91f66fecaed94fb2fdd19665939d06b8cf976c
ArielFeingold <feingold.ariel@gmail.com>
2018-07-17 14:00:23 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,37 @@
+require 'pry'
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    product = Product.new(product_params)
+    if product.save
+      redirect_to products_path
+    else
+      render new_product_path
+    end
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    product.inventory > 0 ? (@value ="true" ) : (@value = "false")
+    render plain: @value
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :inventory)
+  end
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,3 @@
 class Invoice < ActiveRecord::Base
-
   has_many :orders
 end
