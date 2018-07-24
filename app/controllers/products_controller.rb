951e3671db0c3fb9a9ff742e96033871c0f49263
Chely Ho <adept_inept@yahoo.com>
2018-07-24 01:22:39 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,12 @@
 class ProductsController < ApplicationController
+  def create
+    @product = Product.create(product_params)
+    redirect_to products_path
+  end
 
+  private
+
+  def product_params
+      params.require(:product).permit(:name, :price, :inventory, :description)
+  end
 end
