64dc3acf7609cfdba67869a3b00bd711919c24a1
AspenJames <aspenjames@tqca.org>
2018-07-29 16:12:00 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,43 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def inventory
+    set_product
+    if @product.inventory > 0
+      render :plain => 'true'
+    else
+      render :plain => 'false'
+    end
+  end
+
+  def description
+    set_product
+    render :plain => @product.description
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :price, :inventory, :description)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
+  resources :products
+  get '/inventory/:id' => 'products#inventory'
+  get '/description/:id' => 'products#description'
 end
