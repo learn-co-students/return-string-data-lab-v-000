59acc81ee6fec31b5f1d3fe2e66b07c5b6e9754b
Ruth <github email address>
2018-08-09 10:49:01 -0400

    Done.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
+
+  def availability
+    if self.inventory > 0
+      "Available"
+    else
+      "Sold Out"
+    end
+  end
 end
