1d74e5f24ddfd2d87daee02c20c72be3db937b3a
nmlstyl <joey.michalina.mariano@gmail.com>
2018-08-10 21:15:59 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   resources :orders
   resources :invoices
 
-  resources :products, only: [:index, :new, :show]
+  resources :products, only: [:index, :new, :show, :create]
+
+  get '/products/:id/description', to: 'products#description'
+  get '/products/:id/inventory', to: 'products#inventory'
 end
