072e222bdc1487b727cacd8ab479686c6a61023c
Gingertonic <bethmschofield@gmail.com>
2018-08-14 12:40:50 +1000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
+  get '/products/:id/description', to: 'products#description'
+  get '/products/:id/inventory', to: 'products#inventory'
+  get '/products/:id/stock', to: 'products#stock'
+  get '/products', to: 'products#index'
+  post '/products', to: 'products#create'
 end
