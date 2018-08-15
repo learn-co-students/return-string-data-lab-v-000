d067f18375f0fc57e9e297a79a34c0e70fc53935
EC2 Default User <ec2-user@ip-172-31-32-154.us-east-2.compute.internal>
2018-08-15 15:57:54 +0000

    phase 1

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,33 @@
 class ProductsController < ApplicationController
+    
+    def new
+        @prod = Product.new
+    end
+
+    def create
+        @prod = Product.create(prod_params)
+        redirect_to products_path
+    end
+    
+    def index
+        @prods = Product.all
+    end
+    
+    def inventory
+        @prod = Product.find_by(id: params[:id])
+        a = @prod.inventory > 0
+        render plain: a.to_s
+    end
+    
+    def description
+        @prod = Product.find_by(id: params[:id])
+        render plain: @prod.description
+    end
+    
+    private
+    
+    def prod_params
+        params.require(:product).permit(:name, :price, :description, :inventory)
+    end
 
 end
