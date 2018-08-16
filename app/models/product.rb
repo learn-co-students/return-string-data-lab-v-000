e006632edb26718c1ba7c668c0497bc1a7e93516
Vanessa Toro <vanetoro84@gmail.com>
2018-08-15 21:48:17 -0400

    Done.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
+
+  attr_accessor :inventory
+
+  def self.find_all(id)
+    @name = Product.find(id).name
+    self.where(name: @name)
+  end
 end
