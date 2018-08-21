c6cc14a98fc9c6736f7c6327aa35595fb60c27b7
Dee Race <deerace12@gmail.com>
2018-08-21 19:21:28 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,33 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    if product.availablility == "Available"
+      render plain: true
+    else
+      render plain: false
+    end
+  end
+
+  def create
+    @product = Product.create(product_params)
+    @product.save
+    redirect_to products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :inventory)
+  end 
+
 end
