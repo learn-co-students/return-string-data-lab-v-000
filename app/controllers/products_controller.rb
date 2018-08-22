f8bbbf99547b298fe85ff4ca060af5bbfd7e82ae
github username <purnima.nyc@gmail.com>
2018-08-22 06:19:15 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,36 @@
 class ProductsController < ApplicationController
 
+def new
+    @product = Product.new
+end
+
+def index
+    @products = Product.all
+end
+
+def show
+end
+
+def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to products_path
+end
+
+def inventory
+    product = Product.find(params[:id])
+    render plain: product.inventory > 0 ? true : false
+end
+
+def description
+    product = Product.find(params[:id])
+    render plain: product.description
+end
+
+private
+
+def product_params
+    params.require(:product).permit(:name, :price, :description, :inventory)
+end
+
 end
