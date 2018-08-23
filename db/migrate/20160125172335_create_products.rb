893ac029e92f6fe7f00e58416ad4f4e9544225cb
Razelle McCarrick <razelle@satyaorthopedic.com>
2018-08-22 20:16:56 -0400

    Done.

--- db/migrate/20160125172335_create_products.rb
+++ db/migrate/20160125172335_create_products.rb
@@ -2,6 +2,8 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :name
+      t.string :description
+      t.integer :inventory
       t.integer :price
       t.timestamps null: false
     end
