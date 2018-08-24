91245f0d2b7de01f1eb700f92af9e5b4842103ba
Seb Gertz <sebgertz@gmail.com>
2018-08-24 16:04:38 +0000

    plain text routes for description and inventory added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get 'home', to: 'static#home'
+  get 'products', to: 'products#index'
+  get 'products/new', to: 'products#new'
+  get 'products/:id/description', to: 'products#description'
+  get 'products/:id/inventory', to: 'products#inventory'
+  post 'products', to: 'products#create'
   resources :orders
   resources :invoices
+
 end
