cc19c260e1346c232bb08cf52b5ab1c2c6ea04a9
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-08-24 16:34:31 -0400

    Done.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,8 +5,8 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
-require 'capybara/poltergeist'
-Capybara.javascript_driver = :poltergeist
+require 'selenium/webdriver'
+Capybara.javascript_driver = :selenium_chrome_headless
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
