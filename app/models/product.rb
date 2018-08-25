78526b9869d4f6f6beaa7f87e746e9311e3f4c24
Deborah Seow <deborahseow9@gmail.com>
2018-08-25 18:32:50 -0500

    Done.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
+
+  def available?
+    if self.inventory > 0
+      "true"
+    else
+      "false"
+    end
+  end
 end
