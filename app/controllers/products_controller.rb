f13fae48bafdc80ddbfdc80b41c4bc5085bae885
Mo Al <malbazaz@mba2018.hbs.edu>
2018-09-03 23:38:18 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,8 +14,12 @@ class ProductsController < ApplicationController
   end
 
   def inventory
+    product = Product.find(params[:id])
+    render plain: product.inventory
   end
 
   def description
+    product = Product.find(params[:id])
+    render plain: product.description
   end
 end
