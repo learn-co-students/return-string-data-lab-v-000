c2aa42f5bd84f4806eeb85ca5c71d44e8fbdcf9b
Mo Al <malbazaz@mba2018.hbs.edu>
2018-09-03 23:36:46 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,21 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(name: params[:name], price: params[:price], description: params[:description], inventory: params[:inventory])
+    redirect_to products_path
+  end
+
+  def inventory
+  end
+
+  def description
+  end
 end
