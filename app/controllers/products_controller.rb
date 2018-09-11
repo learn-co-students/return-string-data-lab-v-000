d530477c3dccc48db469218a46cd96c05bdcf043
tsyaeger <tsyaeger@gmail.com>
2018-09-10 21:23:27 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,16 @@
 class ProductsController < ApplicationController
 
+	def index 
+		@products = Product.all
+	end
+
+	def new 
+		@product = Product.new
+	end
+
+	def create
+		
+		redirect_to products_path
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
+  resources :products
+
+  # No route matches {:action=>"create", :controller=>"products", :product=>{:name=>"Test", :price=>"3.99", :inventory=>"12", :description=>"This is a great thing"}}
 end
