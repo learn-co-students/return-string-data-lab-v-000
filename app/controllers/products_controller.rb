8dcf0e36ba7965c6c287c1991594a1085f0ab727
Victoria Meng <nights1ststar@protonmail.com>
2018-09-11 23:19:50 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,8 @@
 class ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
+
   def create
     @product = Product.new(product_params)
     if @product.save
@@ -6,7 +10,27 @@ class ProductsController < ApplicationController
     end
   end
 
+  def index
+    @products = Product.all
+  end
+
+  def inventory
+    if find.inventory > 0
+      render plain: "true"
+    else
+      render plain: "false"
+    end
+  end
+
+  def description
+    render plain: find.description
+  end
+
   private
+
+  def find
+    @product = Product.find(params[:id])
+  end
 
   def product_params
     params.require(:product).permit(:name, :price, :description, :inventory_count)
