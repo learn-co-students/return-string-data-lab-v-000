cd435f0ae0e38c33bb6d3b2b7bc909f3b6dde59a
Monica Tamaru Jones <mntamaru@gmail.com>
2018-09-11 23:48:21 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,39 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all 
+  end
+  
+  def new
+    @product = Product.new
+  end
+  
+  def create
 
+    @product = Product.new(product_params)
+    @product.save
+    
+    redirect_to products_path
+  end
+  
+  def description
+    @product = Product.find(params[:id])
+    render plain: @product.description
+  end
+  
+  def inventory
+    @product = Product.find(params[:id])
+    @answer = ""
+    if @product.inventory != 0
+      @answer = "true"
+    else 
+      @answer = "false"
+    end
+    render plain: @answer
+  end
+  
+  private
+  
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :inventory)
+  end
 end
