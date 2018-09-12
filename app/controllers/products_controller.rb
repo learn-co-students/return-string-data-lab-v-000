d0e6d70d3ee8300281bab02d634514d1362260af
tsyaeger <tsyaeger@gmail.com>
2018-09-11 18:20:14 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,8 +9,19 @@ class ProductsController < ApplicationController
 	end
 
 	def create
-		
+		product = Product.create(product_params)
 		redirect_to products_path
 	end
 
+
+
+	private 
+
+	def product_params 
+		params.require(:product).permit(:name, :price, :inventory, :description)
+	end
+
+
+
 end
+
