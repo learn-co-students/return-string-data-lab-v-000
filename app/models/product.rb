8267a8b7ee8d355bf8f2f6dcf1773e4c1b7ac9e5
github username <github email address>
2018-09-16 19:30:03 -0400

    Done.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
+
+  # attr_accessor :description
+  # attr_accessor :inventory
+
 end
