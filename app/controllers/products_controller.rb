e7fe7c72cf96d53bec1fb9b706d89cd8fdd88212
Dillorom <dabdulloeva@gmail.com>
2018-09-18 17:12:01 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,26 @@
 class ProductsController < ApplicationController
+    def index
+        @products = Product.all
+    end
 
+    def new
+        @product = Product.new
+    end
+
+    def body
+        products = Product.find(params[:id])
+        render plain: product.description
+    end
+
+    def create
+        @product = Product.create(product_params)
+        edirect_to @product
+       
+    end
+
+    private
+
+    def product_params
+        params.require(:product).permit(:name, :description)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
+  get '/products/:id/body', to: 'products#body'
 end
