fbf0259b8447df3c63b18cfc813368358da6687e
github username <github email address>
2018-09-26 21:07:19 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,16 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def body
+    @product = find_product
+  end
+
+  private
+
+  def find_product
+    Product.find_by(id: params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
+  resources :products
+  get '/products/:id/body', to: 'products#body'
 end
