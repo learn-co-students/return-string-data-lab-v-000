dc7b397ff31365610c6103ce080f9a633165be1f
github username <github email address>
2018-09-27 22:08:07 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,9 +10,38 @@ class ProductsController < ApplicationController
     render layout:false
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(product_param)
+    redirect_to products_path
+  end
+
+  def inventory
+    @product = find_product
+    if @product.inventory && @product.inventory != 0
+      render plain: "true"
+    else
+      render plain: "false"
+    end
+  end
+
+  def description
+    @product = find_product
+    if @product.description
+      render plain: @product.description
+    end
+  end
+
   private
 
   def find_product
     Product.find_by(id: params[:id])
+  end
+
+  def product_param
+    params.require(:product).permit(:name, :price, :inventory, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   resources :invoices
   resources :products
   get '/products/:id/body', to: 'products#body'
+  get '/products/:id/inventory', to: 'products#inventory'
+  get '/products/:id/description', to: 'products#description'
 end
