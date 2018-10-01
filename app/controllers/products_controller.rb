36e946112964b7bbf7c38fce79f2427db619edb4
Suchitra Naik <susanaik@gmail.com>
2018-10-01 16:09:58 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,42 @@
 class ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to products_path
+  end
+
+  def description
+    @product = Product.find_by(id: params[:id])
+
+    if @product.inventory == 0
+      render plain: @product.description + " " + "Sold Out"
+    else
+      render plain: @product.description
+    end
+  end
+
+  def inventory
+    @product = Product.find_by(id: params[:id])
+    if @product.inventory==0
+      render plain: "false"
+    else
+      render plain: "true"
+    end
+
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :inventory)
+  end
 
 end
