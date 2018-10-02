fc48fa1311b1ded8c19cf9de85e1e7ec2690e5c2
Aubrey Neimeier <neimeier.1@osu.edu>
2018-10-02 21:35:55 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,21 +1,31 @@
 class ProductsController < ApplicationController
   def index
+    binding.pry
     @products = Product.all
   end
 
-  def create
-    binding.pry
-    @prod = Product.new(product_params)
-    if @prod.save
-      render :index
-    else
-      redirect :new
-    end
+  def new
+    @product = Product.new
   end
 
+  def create
+    @product = Product.create(product_params)
+    render :index
+
+  end
+
+  def inventory
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+
   private
-    def product_params
-      params.require(:product).permit(:name, :description)
-    end
+  def product_params
+      params.require(:product).permit(:name, :price, :inventory, :description)
+  end
 
 end
