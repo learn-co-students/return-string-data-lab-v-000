ba56ae7a99622bb4a77d630c242b960292db4c9c
Jamie Brown <jmbrowncow@gmail.com>
2018-10-03 21:14:11 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,34 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product=Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render 'new'
+    end
+  end
+
+  def description
+    product = Product.find_by_id(params[:id])
+    render plain: product.description
+  end
+
+  def inventory
+      product = Product.find_by_id(params[:id])
+      render plain: product.inventory?
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :description, :inventory, :price)
+  end
 end
