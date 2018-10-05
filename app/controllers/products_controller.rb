d229b8415d185f15fd1930ff8ff233c263129257
Veronica Bustabad <vbustabad@gmail.com>
2018-10-05 01:03:01 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,40 @@
 class ProductsController < ApplicationController
 
+  def description
+    @product = Product.find(params[:id])
+    render plain: @product.description
+  end
+
+  def inventory
+    @product = Product.find(params[:id])
+
+    if @product.inventory > 0
+      render plain: "true", layout: false
+    else
+      render plain: "false", layout: false
+    end
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to products_path
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:name, :price, :description, :inventory)
+    end
 end
