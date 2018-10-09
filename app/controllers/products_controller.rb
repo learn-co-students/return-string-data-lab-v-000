caa7ebc1cb8bdc75a807cfe71212586646f205ce
Katrina Brinson <kbrinso@gmail.com>
2018-10-09 19:00:05 +0000

    still working 3 failing

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,21 @@
 class ProductsController < ApplicationController
+  def new
+    @product = Product.new
+    render new_product_path
+  end
 
+  def create
+    @product = Product.new(name: params[:name], price: params[:price])
+    @product.save
+    redirect_to products_path
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
