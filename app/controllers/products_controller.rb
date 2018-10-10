e3c7db8b9f8d7e1424b2f265b51cb9a4481027c2
Katrina Brinson <kbrinso@gmail.com>
2018-10-10 18:43:20 +0000

    learn ide failed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,10 +10,25 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+  def inventory
+    result = Product.find_by(id: params[:id]).inventory > 0 ? true : false
+    render plain: result
+    # binding.pry
+  end
+
+  def description
+    result = Product.find_by(id: params[:id]).description
+    render plain: result
+  end
+
   def edit
   end
 
   def update
+  end
+
+  def index
+    @products = Product.all
   end
 
   def destroy
