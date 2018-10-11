fa0ad6d1558b160efb4abf019016ab674d9b4757
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-10-11 15:24:02 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   resources :invoices
   resources :products
 
+
   get '/products/:id/description', to: 'products#description'
 
   get '/products/:id/inventory', to: 'products#inventory'
 
+
+  get '/products/:id/description', to: 'posts#description'
 end
