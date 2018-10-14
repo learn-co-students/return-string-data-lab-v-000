e25790f375a56680d9d4b155b027c5c0ec9d9b89
Monica  Karasek <mlkarasek@gmail.com>
2018-10-14 00:29:50 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,32 @@
 class ProductsController < ApplicationController
 
+  def index
+       @products = Product.all
+   end
+
+    def create
+       @product = Product.create(post_params)
+       @product.save
+       redirect_to products_path
+   end
+
+    def new
+       @product = Product.new
+   end
+
+    def description
+       product = Product.find(params[:id])
+       render plain: product.description
+   end
+
+    def inventory
+       product = Product.find(params[:id])
+       render plain: product.inventory > 0 ? true : false
+   end
+   
+    private
+
+    def post_params
+       params.require(:product).permit(:name, :price, :description, :inventory)
+   end
 end
