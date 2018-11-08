db2982d00d1c26a80887616164b048efeb0edcf9
asabogal <asabogal.git@gmail.com>
2018-11-07 22:59:00 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,38 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def description
+    @product = Product.find(params[:id])
+    render plain: @product.description
+  end
+
+  def inventory
+    @product = Product.find(params[:id])
+    render plain: @product.inventory > 0 ? true : false
+  end
+
+  def create
+    @product = Product.create(product_params)
+    redirect_to products_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :inventory)
+  end
+
 end
