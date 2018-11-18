9bd05bed52c7bc022eb85b6216e5afa6a6d6b013
JCesana <joshuacesana@gmail.com>
2018-11-17 23:39:20 -0800

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,40 @@
 class ProductsController < ApplicationController
+  include ProductHelper
 
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    if Product.create(product_params)
+      redirect_to products_path
+    else
+      render 'new'
+    end
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    render plain: in_stock?(product)
+  end
+
+  def availability
+    product = Product.find(params[:id])
+    render plain: product.availability
+  end 
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :inventory)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  get 'home', to: 'static#home'
   resources :orders
   resources :invoices
+  resources :products
+
+  get 'home', to: 'static#home'
+
+  get 'products/:id/description', to: 'products#description'
+
+  get 'products/:id/inventory', to: 'products#inventory'
+
+  get 'products/:id/availability', to: 'products#availability'
 end
