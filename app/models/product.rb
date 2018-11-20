08ef248692eef7e4d4e7e401f1ae5d851c4ad1b1
codeesh <vineshpersaud@gmail.com>
2018-11-20 18:47:02 -0500

    Done.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,14 @@
 class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
+
+  def available? 
+    if self.inventory > 1 
+      "true"
+    else
+      "false"
+    end
+  end
+
+
 end
