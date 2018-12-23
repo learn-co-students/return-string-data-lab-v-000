c7660ee09a2335e40bedb05adf4268aac9b9170c
github username <github email address>
2018-12-23 15:56:08 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,19 @@
 class ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
 
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new(products_params)
+    @product.save
+  end
+
+  private
+  def products_params
+     params.require(:project).permit(:name, :price, :description, :inventory)
+  end
 end
