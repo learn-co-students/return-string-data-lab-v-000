f61bf05dc60cc4499aaec74124e0f85cb0544ad3
github username <github email address>
2018-12-27 16:40:17 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,22 @@
+require 'pry'
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    #binding.pry
+  end
 
+  def create
+    #binding.pry
+    @product = Product.create(product_params)
+    @product.save
+    redirect_to products_path
+  end
+
+  def show
+
+  end
+private
+  def product_params
+    params.require(:product).permit(:name, :price, :inventory, :description)
+  end
 end
