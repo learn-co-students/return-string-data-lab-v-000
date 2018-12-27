141b9c745ae692134dd0803f98ab618a00155247
stazman <cadistasio@gmail.com>
2018-12-27 12:04:52 -0600

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,21 @@
 class ProductsController < ApplicationController
 
+    def index
+        @products = Product.all
+    end
+
+    def new
+        @product = Product.new
+    end
+
+    def create
+        @product = Product.create(product_params)
+        redirect_to 'products#index'
+    end
+    
+    private
+
+  def product_params
+    params.require(:product).permit(:name, :price)
+  end 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
+
+  def description
+  end
+
+  def inventory
+  end
 end
