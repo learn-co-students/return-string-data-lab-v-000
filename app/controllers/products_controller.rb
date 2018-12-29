3c38f2c1b3071bfb97f32cc481d3c0271862e9a3
Carol Greene <carolgreene3@gmail.com>
2018-12-29 16:14:33 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,8 +9,7 @@ class ProductsController < ApplicationController
   end
 
   def create
-    product = Product.create(product_params)
-    product.save
+    @product = Product.create(product_params)
     redirect_to products_path
   end
 
