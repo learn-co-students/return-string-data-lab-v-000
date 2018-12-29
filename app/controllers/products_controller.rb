4fc2f67478572b7d28cc814fc61bbe7945149486
Jayon Kim <jpkim921@gmail.com>
2018-12-29 18:57:54 -0500

    done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,49 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    Product.create(product_params)
+    redirect_to products_path
+  end
+
+  def body
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+
+    if product.inventory > 0
+      render plain: true
+    else
+      render plain: false
+    end
+
+  end
+
+  def description
+    product = Product.find(params[:id])
+
+    if product.description
+      render plain: product.description
+    else
+      render plain: "Need description"
+    end
+
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :inventory, :price)
+  end
+
 end
