c2601e2091c31f9806bc337823db4f9159244f10
ElleeB <laura.trager@gmail.com>
2019-01-09 19:36:40 -0500

    Add products#create plus route

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,18 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    if @product = Product.create(product_params)
+      raise @product.inspect
+    else
+      render "form"
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :inventory_count, :description)
+  end
 end
