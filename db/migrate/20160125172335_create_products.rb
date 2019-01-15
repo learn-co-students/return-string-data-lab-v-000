1fd4219f70645b59005fdc8c3b93fa6ba6630a3d
AlexFriedland <friedlandaj@gmail.com>
2019-01-15 14:54:08 -0500

    reseed with order model

--- db/migrate/20160125172335_create_products.rb
+++ db/migrate/20160125172335_create_products.rb
@@ -3,6 +3,8 @@ class CreateProducts < ActiveRecord::Migration
     create_table :products do |t|
       t.string :name
       t.integer :price
+      t.string :description
+      t.integer :count
       t.timestamps null: false
     end
   end
