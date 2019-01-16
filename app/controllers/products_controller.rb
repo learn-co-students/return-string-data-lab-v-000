db616fb546d1c916570e48b533c334eab0d88208
Nhinh Dao <nhinhdao.vn@gmail.com>
2019-01-15 22:48:13 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,32 @@
 class ProductsController < ApplicationController
+  
+  def index
+    @products = Product.all
+    # byebug
+  end
+  
+  def description
+    get_product
+    render plain: @product.description
+  end
+  
+  def inventory
+    get_product
+    render plain: @product.inventory > 0 ? 'true' : 'false'
+  end
+  
+  def new
+    @product = Product.new
+  end
 
+  def create
+    @product = Product.create(name: params[:product][:name], description: params[:product][:description], inventory: params[:product][:inventory])
+    redirect_to '/products'
+  end
+
+  private
+  
+  def get_product
+    @product = Product.find(params[:id])
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
+  
+  # def description
+  #   self.description
+  # end
+  
+  # def inventory
+  #   self.inventory
+  # end
 end
