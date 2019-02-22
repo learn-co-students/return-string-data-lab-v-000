daff5a18429d394d113a06d97ab0eadf8c5bbbef
Lucy Wall <lhwall@gmail.com>
2019-02-22 19:00:48 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,22 @@
 class ProductsController < ApplicationController
 
+def body
+
+  product = Product.find(params[:id])
+  render plain: product.description
+end
+
+def create
+  @product = Product.create(product_params)
+  @product.save
+  redirect_to products_path
+end
+
+
+private
+
+def product_params
+  params.require(:product).permit(:name, :price, :inventory, :description)
+end
+
 end
