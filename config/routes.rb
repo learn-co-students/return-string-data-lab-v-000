a1218839ed65f1d3747aae45503156a3b69a3bae
amitnyc83 <amit_nyc@live.com>
2019-02-25 14:56:29 -0500

    returns a string data/ All test passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get 'home', to: 'static#home'
+  resources :products
   resources :orders
   resources :invoices
+
+  get 'products/:id/inventory', to: 'products#inventory'
+  get 'products/:id/description', to: 'products#description'
+
 end
