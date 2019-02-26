694eb744885a4acba24fa71af4db4a167a02ea5a
epic0448 <samuelogundiran@Yahoo.com>
2019-02-26 14:49:50 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,12 @@
 class ProductsController < ApplicationController
 
+def index
+    @products = Product.all
+end
+
+    def create
+     @products = Product.new
+
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home', to: 'static#home'
+  get '/orders/:id/body' , to: 'products#create'
   resources :orders
   resources :invoices
 end
