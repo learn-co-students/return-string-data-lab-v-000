8ae60d5ecbc2f6b8c1a8867d204c7fb1d73de769
freeWillee <will.ik.lee@gmail.com>
2019-03-08 14:50:51 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,13 @@
 class ProductsController < ApplicationController
+    def description
+        product = Product.find(params[:id])
+        
+    end
 
+    def inventory
+    end
+
+    def index
+        @products = Product.all
+    end
 end
