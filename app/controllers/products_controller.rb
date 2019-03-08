60c4ad12735d4685f96e424ce2261448237ec14f
Kkelly22 <kk504408@gmail.com>
2019-03-07 23:04:09 -0500

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,60 @@
 class ProductsController < ApplicationController
+	before_action :set_product, only: [:show, :edit, :update]
+
+	def index
+		@products = Product.all
+	end
+
+	def show
+	end
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.create(product_params)
+		@product.save
+		redirect_to products_path
+	end
+
+	def edit
+	end
+
+	def update
+		@product.update(product_params)
+		redirect_to product_path(@product)
+	end
+
+	def description
+		product = Product.find(params[:id])
+		render plain: product.description
+	
+	#	if product.inventory > 0
+	#		descrip = ".  Inventory is Available."
+	#	else
+	#		descrip = ".  Inventory is Sold Out."
+	#	end
+	end
+
+	def inventory
+		@product = Product.find(params[:id])
+		if @product.inventory && @product.inventory > 0
+			render plain: "true"
+		else
+			render plain: "false"
+		end
+	end
+
+
+	private
+
+		def set_post
+    		@product = Product.find(params[:id])
+  		end
+
+		def product_params
+      		params.require(:product).permit(:name, :price, :inventory, :description)
+   		end
 
 end
