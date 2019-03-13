12ba0ee8080766d7b03bf517d1673c8e0b69d8b5
BrentBauman86 <brentbauman86@gmail.com>
2019-03-13 04:56:47 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,25 @@
 class ProductsController < ApplicationController
 
+    def index
+        raise 
+        @products = Product.all
+    end
+
+    def show 
+        @products = Product.find_by(id: params[:id])    
+    end
+
+    def new
+        @product = Product.new 
+    end
+
+    def create
+        @product = Product.create(product_params)
+        @product.save
+        redirect_to product_path(@product)
+    end
+
+    def product_params
+        params.require(:product).permit(:name, :price)
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
+  root 'products#index'
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
+  resources :products 
 end
