3718e0add1e2274d9217ad2fa7015d4fc0a47a8c
BrentBauman86 <brentbauman86@gmail.com>
2019-03-14 04:31:05 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,25 +1,31 @@
 class ProductsController < ApplicationController
 
     def index
-        raise 
-        @products = Product.all
+      @products = Product.all
     end
-
-    def show 
-        @products = Product.find_by(id: params[:id])    
-    end
-
+  
     def new
-        @product = Product.new 
+      @product = Product.new
     end
-
+  
     def create
-        @product = Product.create(product_params)
-        @product.save
-        redirect_to product_path(@product)
+      @product = Product.create(product_params)
+      @product.save
+      redirect_to products_path
     end
-
+  
+    def description
+      product = Product.find(params[:id])
+      render plain: product.description
+    end
+  
+    def inventory
+      render inline: "<%= Product.find(params[:id]).inventory >= 1 ? 'true' : 'false' %>"
+    end
+  
+  private
+  
     def product_params
-        params.require(:product).permit(:name, :price)
-      end
-end
+      params.require(:product).permit(:name, :description, :price, :inventory)
+    end
+  end
