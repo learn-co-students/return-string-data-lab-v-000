b1e324440d4f1ddba090667c872001a970c4977e
ryanwevans <ryanwevans@icloud.com>
2019-04-03 23:00:02 -0700

    moved index page to partial; add #availability and #inventory? to Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,20 @@
 class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
+
+  def inventory?
+    if self.inventory > 0
+      return "true"
+    else
+      return "false"
+    end
+  end
+
+  def availability
+    if self.inventory > 0
+      return "Available"
+    else
+      return "Sold Out"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
+  resources :products
+
+  get '/products/:id/description', to: 'products#description'
+  get '/products/:id/inventory', to: 'products#inventory'
+  get '/products/:id/inventory_status', to: 'products#inventory_status'
+
 end
