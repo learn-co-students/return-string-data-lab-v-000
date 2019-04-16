5d626fe235cb8a9f2f9f5996bf9d93621c8576f8
charlie  lee <leetochia@gmail.com>
2019-04-16 16:49:39 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,36 +1,38 @@
 class ProductsController < ApplicationController
- def index
-   @products = Product.all
- end
+	before_action :set_product, only: [:show, :edit, :update, :inventory, :description]#this was the differences
 
- def new
-   @product = Product.new
- end
+	def description
+		render plain: @product.description
+	end
 
- def create
-    @product = Product.new
-    @product.name = params[:product][:name]
-    @product.price = params[:product][:price]
-    @product.description = params[:product][:description]
-    @product.inventory = params[:product][:inventory]
-    @product.save
-    redirect_to products_path
- end
- def description
-   product = Product.find(params[:id])
-  render plain: product.description
-  end
+	def inventory
+		if (@product.inventory.nil?)
+			render plain: false
+		else
+			render plain: @product.inventory > 0 ? true : false
+		end
+	end
 
-  def inventory
-    product = Product.find(params[:id])
-    if product.inventory > 0
-      render plain: "true"
-    else
-      render plain: "false"
-    end
-  end
-  private
-  def product_params
-    params.require(:product).permit(:name, :description, :inventory, :price)
-  end
+	def index
+		@products = Product.all
+	end
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+	  Product.create(product_params)
+		redirect_to products_path
+	end
+
+private
+
+	def set_product
+		@product = Product.find(params[:id])# this was missing
+	end
+
+	def product_params
+		params.require(:product).permit(:name, :description, :inventory, :price)
+	end
 end
