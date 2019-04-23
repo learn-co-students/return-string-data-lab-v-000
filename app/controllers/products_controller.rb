2596bb1f0953d7c0cf543bea2bcb6bd731ad39a4
Naomi Dennis <njdennis94@gmail.com>
2019-04-23 19:20:45 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,15 @@
 class ProductsController < ApplicationController
 
+  def index 
+     @products = Product.all 
+  end 
+  def create
+     Product.create!(product_params)
+     redirect_to products_path 
+  end 
+   private 
+   
+    def product_params
+      params.permit(:name, :price, :inventory, :description)
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
+  resources :products
 end
