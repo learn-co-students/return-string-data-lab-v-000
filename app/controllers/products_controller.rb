b90959dad9a18343420826ab88c2fa2c7e6a2521
Rachel Wyatt <rw126@evansville.edu>
2019-05-06 20:26:05 -0400

    create products index page and index method in products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,11 +3,15 @@ class ProductsController < ApplicationController
     def create 
         @product = Product.create(product_params)
         @product.save 
-        redirect_to product_path(@product)
+        redirect_to products_path
     end
 
     def new 
         @product = Product.new
+    end
+
+    def index 
+        @products = Product.all
     end
 
 
