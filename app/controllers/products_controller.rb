a8859601d9ac658cc57a945cb663be2d11e9fe5f
Rachel Wyatt <rw126@evansville.edu>
2019-05-06 20:18:55 -0400

    add create method and product_params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,16 @@
 class ProductsController < ApplicationController
 
+    def create 
+        @product = Product.create(product_params)
+        @product.save 
+        redirect_to product_path(@product)
+    end
+
+
+    private 
+
+    def product_params 
+        params.require(:post).permit(:description, :inventory, :name, :price)
+    end
+
 end
