af6d3fb459a79d53b440f313ce6a4b9f0a6f96e2
Rachel Wyatt <rw126@evansville.edu>
2019-05-06 20:04:54 -0400

    add description and inventory to Product model

---
+++ db/migrate/20190507000306_add_description_and_inventory_to_product.rb
@@ -0,0 +1,6 @@
+class AddDescriptionAndInventoryToProduct < ActiveRecord::Migration[5.0]
+  def change
+    add_column :products, :description, :string
+    add_column :products, :inventory, :integer
+  end
+end
