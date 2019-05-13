c4a4d49f10e0d0c4fa15bd65b158cdd583737316
liquidsword <audenrobertson@gmail.com>
2019-05-12 21:59:53 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
+  resources :products, only: [:index, :new, :create]
+  get '/products/:id/description', to: 'products#description'
+  get '/products/:id/inventory', to: 'products#inventory'
 end
