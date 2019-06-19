757bdd7020fc66e1ddc0df3621b6f399395333e7
inkanimal <theatomproject@gmail.com>
2019-06-18 21:44:52 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,40 @@
 class ProductsController < ApplicationController
+    before_action :set_product, only: [:inventory, :description]
+
+    def index
+        @products = Product.all
+    end
+
+    def create 
+        @product = Product.new(product_params)
+        if @product.save 
+         redirect_to products_path(@path)
+        end
+    end
+
+    def description 
+        product = Product.find(params[:id])
+        render plain: product.description
+    end
+
+    def inventory 
+        product = Product.find(params[:id])
+        if product.inventory > 0
+            render text: "true"
+        else
+            render text: "false"
+        end
+    end
+
+
+    private 
+
+    def product_params 
+        params.require(:product).permit(:description, :inventory, :name, :price)
+    end
+
+    def set_product
+        @product = Product.find(params[:id])
+    end
 
 end
