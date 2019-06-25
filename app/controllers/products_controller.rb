b595ed932826e7b65435960cdc278041d35f2e4e
Erin Green <erin.y.green@gmail.com>
2019-06-25 18:12:43 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,26 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
+  def new
+
+  end
+
+  def create
+    @product = Product.create(product_params)
+    @product.save
+    redirect_to product_path(@product)
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render :plain product.description
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description)
+  end
+
 end
