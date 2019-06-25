596526cf18cc00d0da7c7ee714d06c101a6b5f1c
Erin Green <erin.y.green@gmail.com>
2019-06-25 18:30:17 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,16 +11,25 @@ class ProductsController < ApplicationController
   def create
     @product = Product.create(product_params)
     @product.save
-    redirect_to product_path(@product)
+    redirect_to products_path
   end
 
   def description
     product = Product.find(params[:id])
-    render :plain product.description
+    render plain: product.description
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    if product.inventory > 0
+      render plain: true
+    else
+      render plain: false
+    end
   end
 
   def product_params
-    params.require(:product).permit(:name, :price, :description)
+    params.require(:product).permit(:name, :price, :description, :inventory)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :invoices
   resources :products
 
-  get '/products/:id/description', to: 'products/description'
+  get '/products/:id/description', to: 'products#description'
+  get '/products/:id/inventory', to: 'products#inventory'
 end
