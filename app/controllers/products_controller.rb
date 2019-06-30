39a3de098e930acea9c3644db1db09d2ea36039a
Rebecca Strauss <rstrauss127@gmail.com>
2019-06-30 18:38:52 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,30 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    end
+  end
+
+  def description
+    @product = Product.find(params[:id])
+    render plain: @product.description
+  end
+
+  def inventory
+    @product = Product.find(params[:id])
+    render plain: @product.inventory?
+  end
+private
+    def product_params
+      params.require(:product).permit(:name, :description, :inventory, :price)
+    end
 end
