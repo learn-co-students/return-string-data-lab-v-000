63648f642b912719363b2ce7b6e6dfe1e6502b33
lpzraf <lpz.raf@gmail.com>
2019-07-02 23:20:51 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,28 @@
 class ProductsController < ApplicationController
 
+  def create
+    @product = Product.create(post_params)
+    @product.save
+    redirect_to '/products'
+  end
+
+  def inventory
+
+  end
+
+  def description
+
+  end
+
+  def body
+  product = Product.find(params[:id])
+  render plain: product.description
+end
+
+private
+
+  def post_params
+    params.require(:product).permit(:ordered_products, :orders, :inventory, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'home', to: 'static#home'
+
   resources :orders
   resources :invoices
+
+  post '/products',  to: 'products#create'
+  get '/products',  to: 'products#inventory'
+  get '/products',  to: 'products#description'
 end
