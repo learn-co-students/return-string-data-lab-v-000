05e85ef92030a004225f3dd222afac7a13eed04b
christianavdm <christianavdm@gmail.com>
2019-07-31 17:31:41 -0700

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,23 @@
 class ProductsController < ApplicationController
+    
+    def new
+        @product = Product.new 
+    end
+    
+    def index 
+        @products = Product.all
+    end
+    
+    def description 
+        product = Product.find(params[:id])
+        render plain: product.description
+    end
 
+    def inventory 
+        product = Product.find(params[:id]) 
+
+        @inventory = !!product.inventory 
+
+        render 'inventory', :layout => false
+    end
 end
