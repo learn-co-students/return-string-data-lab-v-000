e04ef8c79575b29089d72cb6660dae3a2aaeed73
Jonathan Luk <jluk87@gmail.com>
2019-08-30 15:20:30 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,38 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:description, :availability]
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render new_product_path
+    end
+  end
+
+  def description
+    render plain: @product.description
+  end
+
+  def availability
+    render plain: !!@product.inventory
+  end
+
+  private
+
+  def set_product
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :inventory)
+  end
 
 end
