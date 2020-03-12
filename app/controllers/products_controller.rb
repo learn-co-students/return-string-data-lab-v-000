deb9b7e0486324d0abb56ee50c412d5c13e2c63e
Kat Tochowicz <ktochow1@binghamton.edu>
2020-03-12 18:30:30 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,39 @@
 class ProductsController < ApplicationController
 
+  def create
+    product = Product.create(product_params)
+    # @product.name = params[:product][:name]
+    # @product.price = params[:product][:price]
+    redirect_to products_path
+  end
+
+  def inventory
+    # binding.pry
+    @product = Product.find_by(id: params[:id])
+    if @product.inventory < 1
+      response = "false"
+    else
+      response = "true"
+    end
+    render plain: response
+  end
+
+  def description
+    product = Product.find_by(id: params[:id])
+    render plain: product.description
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def index
+    @products = Product.all
+  end
+
+
+private
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :inventory)
+  end
 end
